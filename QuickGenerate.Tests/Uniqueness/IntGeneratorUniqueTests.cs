87895029396a319173e5da8ee12c61868838eee9
Mark Meyers <kilfour@gmail.com>
2011-08-01 18:38:08 +0200

    simple uniqueness strategy

---
+++ QuickGenerate.Tests/Uniqueness/IntGeneratorUniqueTests.cs
@@ -0,0 +1,26 @@
+﻿using QuickGenerate.Modifying;
+using QuickGenerate.Primitives;
+using QuickGenerate.Uniqueness;
+using Xunit;
+
+namespace QuickGenerate.Tests.Uniqueness
+{
+    public class IntGeneratorUniqueTests
+    {
+        [Fact]
+        public void SetOfOneElement()
+        {
+            var generator = new IntGenerator(42, 42).Unique();
+            generator.GetRandomValue();
+            Assert.Throws<HeyITriedFiftyTimesButCouldNotGetADifferentValue>(() => generator.GetRandomValue());
+        }
+
+        [Fact]
+        public void SetOfOneThreeElements()
+        {
+            var generator = new IntGenerator(42, 45).Unique();
+            3.Times(() => generator.GetRandomValue());
+            Assert.Throws<HeyITriedFiftyTimesButCouldNotGetADifferentValue>(() => generator.GetRandomValue());
+        }
+    }
+}
---
+++ quickgenerate/Uniqueness/UniqueGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using QuickGenerate.Modifying;
+
+namespace QuickGenerate.Uniqueness
+{
+    public class UniqueGenerator<T> : Generator<T>
+    {
+        private readonly IGenerator<T> generator;
+        private readonly List<T> previouslyOn = new List<T>();
+        public UniqueGenerator(IGenerator<T> generator)
+        {
+            this.generator = generator;
+        }
+
+        public override T GetRandomValue()
+        {
+            
+            for (int i = 0; i < 50; i++)
+            {
+                var result = generator.GetRandomValue();
+                if(!previouslyOn.Contains(result))
+                {
+                    previouslyOn.Add(result); 
+                    return result;
+                }
+                previouslyOn.Add(result);
+            }
+            throw new HeyITriedFiftyTimesButCouldNotGetADifferentValue();
+        }
+    }
+
+    public static class UniqueGeneratorFactory
+    {
+        public static UniqueGenerator<T> Unique<T>(this IGenerator<T> generator)
+        {
+            return new UniqueGenerator<T>(generator);
+        }
+    }
+}

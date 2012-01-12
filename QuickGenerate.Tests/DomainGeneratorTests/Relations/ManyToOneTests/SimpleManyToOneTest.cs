8174fe8822a6faa5655b8962a9840776ecb264a6
Mark Meyers <kilfour@gmail.com>
2012-01-12 16:20:57 +0100

    bugfix

---
+++ QuickGenerate.Tests/DomainGeneratorTests/Relations/ManyToOneTests/SimpleManyToOneTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace QuickGenerate.Tests.DomainGeneratorTests.Relations.ManyToOneTests
+{
+    public class SimpleManyToOneTest
+    {
+        [Fact]
+        public void Works()
+        {
+            var domainGenerator =
+                new DomainGenerator()
+                    .ManyToOne<Child, Parent>(2, (c, p) => p.Add(c));
+
+            var child = domainGenerator.One<Child>();
+            Assert.NotNull(child);
+            Assert.NotNull(child.MyParent);
+            Assert.Equal(2, child.MyParent.Children.Count);
+            Assert.Contains(child, child.MyParent.Children);
+        }
+
+        public class Parent
+        {
+            public List<Child> Children { get; set; }
+
+            public Parent()
+            {
+                Children = new List<Child>();
+            }
+
+            public void Add(Child child)
+            {
+                Children.Add(child);
+                child.MyParent = this;
+            }
+        }
+
+        public class Child
+        {
+            public Parent MyParent { get; set; }
+        }
+    }
+}

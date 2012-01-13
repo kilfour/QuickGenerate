7af0fe127402f27302345f7c7f3335d6e896d0a1
Mark Meyers <kilfour@gmail.com>
2012-01-13 10:13:12 +0100

    missing file

---
+++ quickgenerate/DomainGeneratorImplementation/ConstructorGeneratorOptions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace QuickGenerate.DomainGeneratorImplementation
+{
+    public interface IConstructorGeneratorOptions
+    {
+        Type[] GetParameterTypes();
+    }
+
+    public class ConstructorGeneratorOptions<TBase> : IConstructorGeneratorOptions
+    {
+        private readonly List<Type> parmeterTypes = new List<Type>();
+        public ConstructorGeneratorOptions<TBase> Construct<TDerived>()
+        {
+            parmeterTypes.Add(typeof(TDerived));
+            return this;
+        }
+
+        public Type[] GetParameterTypes()
+        {
+            return parmeterTypes.ToArray();
+        }
+    }
+
+    
+}

a8f2f1b89d62d97ecc750f854fbb7b013ab64f46
Mark Meyers <kilfour@gmail.com>
2012-01-17 09:28:36 +0100

    mo' tests

---
+++ QuickGenerate.Tests/DomainGeneratorTests/Components/AvoidingRecursion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using QuickGenerate.DomainGeneratorImplementation;
+using Xunit;
+
+namespace QuickGenerate.Tests.DomainGeneratorTests.Components
+{
+    public class AvoidingRecursion
+    {
+        [Fact]
+        public void Works()
+        {
+            var domainGenerator =
+                new DomainGenerator()
+                    .Component<Something>();
+
+            Assert.Throws<RecursiveRelationDefinedException>(() => domainGenerator.One<Something>());
+        }
+
+        public class Something
+        {
+            public Something MySomething { get; set; }
+        }
+    }
+}
